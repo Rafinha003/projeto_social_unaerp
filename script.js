04b9e8f857b452829eed2f91ad0325e3934499ab
Rafael Martins Nascimento <rafael.mnascimento@totvs.com.br>
2024-05-31 15:13:44 -0300

    criando um arquivo js

---
+++ script.js
@@ -0,0 +1,9 @@
+function atualizarContador() {
+    var contadorElemento = document.getElementById('contadorValor');
+    var contadorAtual = parseInt(contadorElemento.textContent);
+    contadorAtual++;
+    contadorElemento.textContent = contadorAtual;
+  }
+  
+  setInterval(atualizarContador, 12000); 
+  
