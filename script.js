0bf9167c94b1c417bf0f96d61e2dbf818077fed4
Rafael Jorge <rafaelcamillojorge@gmail.com>
2024-06-02 22:18:30 -0300

    refatorando tela

--- script.js
+++
@@ -1,8 +0,0 @@
-function atualizarContador() {
-  var contadorElemento = document.getElementById("contadorValor");
-  var contadorAtual = parseInt(contadorElemento.textContent);
-  contadorAtual++;
-  contadorElemento.textContent = contadorAtual;
-}
-
-setInterval(atualizarContador, 12000);
